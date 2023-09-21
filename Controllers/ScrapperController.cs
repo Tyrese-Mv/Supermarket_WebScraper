772fb282995acbba0909248ea9029750a284fcc9
Tyrese-Mv <musawenkosi106@gmail.com>
2023-09-21 09:36:13 +0200

    Add project files.

---
+++ Controllers/ScrapperController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebScrapper.Models;
+
+
+namespace WebScrapper.Controllers
+{
+    public class ScrapperController : Controller
+    {
+        public IActionResult Index()
+        {
+            string url = "https://www.game.co.za/Home-Appliances/Kitchen-Large-Appliances/Built-In-Ovens/l/c/G3056";
+            string className = "css-901oao css-cens5h r-1tvphgr r-ukjfwf r-1b43r93 r-14yzgew r-14gqq1x r-17j37da";
+            var scraper = new ScrapeGame();
+            Task<List<string>> scrapeTask = scraper.ScrapeWebsite(url, className);
+            scrapeTask.Wait();
+
+            List<string> extractedTextList = scrapeTask.Result;
+
+            ViewBag.ExtractedText = extractedTextList;
+            return View();
+        }
+    }
+
+}
---
+++ Models/ScrapeGame.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using HtmlAgilityPack.CssSelectors;
+using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace WebScrapper.Models;
+public class ScrapeGame
+{
+    public async Task<List<string>> ScrapeWebsite(string url, string className)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            string htmlContent = await response.Content.ReadAsStringAsync();
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlContent);
+
+            List<string> extractedTextList = new();
+
+            foreach ( HtmlNode element in htmlDocument.DocumentNode.Descendants().Where(d => d.HasClass(className)))
+            {
+                extractedTextList.Add(element.InnerText);
+            }
+
+            //var element = htmlDocument.DocumentNode.SelectSingleNode("//h1");
+
+            //string? extractedText = element?.InnerText;
+
+            return extractedTextList;
+        }
+    }
+}
